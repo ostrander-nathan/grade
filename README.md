9594f1c66e348986a27d59ff087d3d52b080ede6
Nathan Ostrander <ostrander_nathan@yahoo.com>
2017-04-26 14:21:13 -0500

    clean & readme

---
+++ README.md
@@ -0,0 +1,24 @@
+# Grades
+
+### Description: 
+
+* Introduction into Javascript 
+
+### Final Result:
+> Console Application showing Javascript
+
+### Take-home Lesson:
+
+> Using for loops and if statments over array to sort data
+
+
+### How to run (Node must be installed on your machine):
+```
+git clone https://github.com/ostrander-nathan/grades
+npm install http-server -g
+http-server -p 8080
+```
+
+
+### Contributors:
+[Nathan Ostrander](https://github.com/ostrander-nathan)
--- grades.js
+++ grades.js
@@ -5,35 +5,34 @@ var cC = 0;
 var dD = 0;
 var fF = 0;
 var highestGrade = 0;
-var lowestGrade =  100;
+var lowestGrade = 100;
 for (var i = 0; i < scores.length; i++) {
-	 if ((50 < scores[i]) && (scores[i] <=60)) {
-	 		fF += 1;
-	 }
-	 if ((61 < scores[i]) && (scores[i] <= 70)) {
-	 		dD += 1;
-	 }
-	  if ((71 < scores[i]) && (scores[i] <= 80)) {
-	 		cC += 1;
-	 }
-	 if ((81 < scores[i]) && (scores[i] <= 90)) {
-	 		bB += 1;
-	 }
-	 if ((91 < scores[i]) && (scores[i] <= 100)) {
-	 		aA += 1;
-	 }
-	if (scores[i] > highestGrade) {
-		highestGrade = scores[i];
-	}
-	if (scores[i] < lowestGrade) {
-	lowestGrade = scores[i];
-	}
+    if ((50 < scores[i]) && (scores[i] <= 60)) {
+        fF += 1;
+    }
+    if ((61 < scores[i]) && (scores[i] <= 70)) {
+        dD += 1;
+    }
+    if ((71 < scores[i]) && (scores[i] <= 80)) {
+        cC += 1;
+    }
+    if ((81 < scores[i]) && (scores[i] <= 90)) {
+        bB += 1;
+    }
+    if ((91 < scores[i]) && (scores[i] <= 100)) {
+        aA += 1;
+    }
+    if (scores[i] > highestGrade) {
+        highestGrade = scores[i];
+    }
+    if (scores[i] < lowestGrade) {
+        lowestGrade = scores[i];
+    }
 }
-console.log("student grade F is",fF);
-console.log("student grade D is",dD);
-console.log("student grade C is",cC);
-console.log("student grade B is",bB);
-console.log("student grade A is",aA);
-
-console.log("student with low grade",lowestGrade);
-console.log("student with highest grade",highestGrade);
+console.log("student grade F is", fF);
+console.log("student grade D is", dD);
+console.log("student grade C is", cC);
+console.log("student grade B is", bB);
+console.log("student grade A is", aA);
+console.log("student with low grade", lowestGrade);
+console.log("student with highest grade", highestGrade);
